e0c80bfb697ca392fa0ae69aaa1ddc315cb2ed91
Afzal <afzalmomin2003@gmail.com>
2024-11-19 15:52:39 +0530

    app working

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 import sys
-from encrypter import Encrypter
-from decrypter import Decrypter
+from Encrypter import Encrypter
+from Decrypter import Decrypter
 
-encrypter = Encrypter()
-decrypter = Decrypter()
+
+class app():
+    def __init__(self,operation,file,passwd):
+        self.operation = operation
+        self.file = file
+        self.passwd = passwd
+
+    def run(self):
+        try:
+            if self.operation == "encrypt":
+                encrypter = Encrypter(self.file,self.passwd)
+                encrypter.encrypt()
+            elif self.operation == "decrypt":
+                decrypeter = Decrypter(self.file,self.passwd)
+                decrypeter.decrypt()
+            else:
+                raise ValueError(f"Invalid operation specified : {self.operation}")
+        except Exception as e:
+            print("error:",e)
+
+
+if __name__ == '__main__':
+    operation = sys.argv[1]
+    file = sys.argv[2]
+    passwd = sys.argv[3]
+    app = app(operation=operation,file=file,passwd=passwd)
+    app.run()
